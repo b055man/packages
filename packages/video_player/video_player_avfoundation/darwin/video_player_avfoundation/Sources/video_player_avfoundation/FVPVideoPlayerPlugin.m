e0ef072e16e7ed9281e391d6e150b4d0688a9738
Paweł Kluk <pawel.kluk@motorolasolutions.com>
2025-07-01 13:51:25 +0200

    rework

--- packages/video_player/video_player_avfoundation/darwin/video_player_avfoundation/Sources/video_player_avfoundation/FVPVideoPlayerPlugin.m
+++ packages/video_player/video_player_avfoundation/darwin/video_player_avfoundation/Sources/video_player_avfoundation/FVPVideoPlayerPlugin.m
@@ -689,26 +689,26 @@ NS_INLINE CGFloat radiansToDegrees(CGFloat radians) {
 
   [_player.currentItem selectMediaOption:option inMediaSelectionGroup:group];
 
-  // NSLog(@"[VideoPlayer] DEBUG: selectMediaOption call completed. Fetching updated tracks...");
+  NSLog(@"[VideoPlayer] DEBUG: selectMediaOption call completed. Fetching updated tracks...");
 
-  // [self fetchAudioTracksWithCompletion:^(NSMutableArray<AudioTrack *> *audioTracks) {
-  //   if (audioTracks) {
-  //       NSLog(@"[VideoPlayer] DEBUG: Fetch completion handler called. Track count: %lu", (unsigned long)audioTracks.count);
-  //       if (self.eventSink) {
-  //           NSLog(@"[VideoPlayer] DEBUG: EventSink is valid. Preparing to send 'audioTracksUpdated' event.");
-  //           NSArray<NSDictionary *> *audioTracksAsMap = [audioTracks valueForKey:@"asMap"];
-  //           NSLog(@"[VideoPlayer] DEBUG: Sending event data: %@", audioTracksAsMap);
-  //           _eventSink(@{
-  //               @"event" : @"audioTracksChanged",
-  //               @"audioTracks" : audioTracksAsMap,
-  //           });
-  //       } else {
-  //           NSLog(@"[VideoPlayer] DEBUG: FAILED - EventSink is nil.");
-  //       }
-  //   } else {
-  //       NSLog(@"[VideoPlayer] DEBUG: FAILED - fetchAudioTracksWithCompletion returned nil tracks.");
-  //   }
-  // }];
+  [self fetchAudioTracksWithCompletion:^(NSMutableArray<AudioTrack *> *audioTracks) {
+    if (audioTracks) {
+        NSLog(@"[VideoPlayer] DEBUG: Fetch completion handler called. Track count: %lu", (unsigned long)audioTracks.count);
+        if (self.eventSink) {
+            NSLog(@"[VideoPlayer] DEBUG: EventSink is valid. Preparing to send 'audioTracksUpdated' event.");
+            NSArray<NSDictionary *> *audioTracksAsMap = [audioTracks valueForKey:@"asMap"];
+            NSLog(@"[VideoPlayer] DEBUG: Sending event data: %@", audioTracksAsMap);
+            _eventSink(@{
+                @"event" : @"audioTracksChanged",
+                @"audioTracks" : audioTracksAsMap,
+            });
+        } else {
+            NSLog(@"[VideoPlayer] DEBUG: FAILED - EventSink is nil.");
+        }
+    } else {
+        NSLog(@"[VideoPlayer] DEBUG: FAILED - fetchAudioTracksWithCompletion returned nil tracks.");
+    }
+  }];
 }
 
 - (void)onTextureUnregistered:(NSObject<FlutterTexture> *)texture {
